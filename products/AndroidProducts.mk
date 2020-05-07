898e87b127623b84f671ec0838db493897b218cd
JLexu <juan.lexu@gmail.com>
2020-05-07 10:15:49 +0000

    fix missing separator

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -34,7 +34,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/rosy/pa_rosy.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
     $(LOCAL_DIR)/X00TD/pa_X00TD.mk \
-    $(LOCAL_DIR)/X01BD/pa_X01BD.mk
+    $(LOCAL_DIR)/X01BD/pa_X01BD.mk \
     device/xiaomi/grus/pa_grus.mk
 
 # SONY OpenDevices
