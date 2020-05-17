a5edb8465603970f1a059400c02c8ec3d8457f98
JLexu <juan.lexu@gmail.com>
2020-05-17 14:00:56 +0000

    fix apn path removed previously

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,9 +1,9 @@
 # Sensitive Phone Numbers list
 #PRODUCT_COPY_FILES += \
-#    vendor/pa/prebuilt/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+    vendor/pa/prebuilt/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
 # World APN list
-#PRODUCT_COPY_FILES += vendor/pa/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+PRODUCT_COPY_FILES += vendor/pa/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
 
 # Telephony packages
 #PRODUCT_PACKAGES += \
@@ -16,8 +16,8 @@
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
-#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-#    net.tethering.noprovisioning=true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    net.tethering.noprovisioning=true
 
 # IMS
 #PRODUCT_PACKAGES += \
